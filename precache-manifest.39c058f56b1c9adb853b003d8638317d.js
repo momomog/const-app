61b8c871db7d8a5fe841af814dcfab83641243a7
SFokin <Momomoqwer1>
2020-09-26 20:03:42 +0400

    Updates

---
+++ precache-manifest.39c058f56b1c9adb853b003d8638317d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80e0b98691802fad81a266eabe9d36af",
+    "url": "/const-app/index.html"
+  },
+  {
+    "revision": "883ae94acc2b458f77df",
+    "url": "/const-app/static/css/3.cecf6617.chunk.css"
+  },
+  {
+    "revision": "0b95d33cd3c66c1e6282",
+    "url": "/const-app/static/css/6.f3e97da9.chunk.css"
+  },
+  {
+    "revision": "04d19908cabc9b54fa3a",
+    "url": "/const-app/static/css/7.9a90d1d1.chunk.css"
+  },
+  {
+    "revision": "e93f1de37f2dbbaeb2a4",
+    "url": "/const-app/static/css/main.966366d2.chunk.css"
+  },
+  {
+    "revision": "879d938197dc06c568a2",
+    "url": "/const-app/static/js/2.bf825117.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/const-app/static/js/2.bf825117.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "883ae94acc2b458f77df",
+    "url": "/const-app/static/js/3.30e02e5f.chunk.js"
+  },
+  {
+    "revision": "5b8b8f21f171d211393d",
+    "url": "/const-app/static/js/4.d923127a.chunk.js"
+  },
+  {
+    "revision": "efeb89138cf0890bbf97",
+    "url": "/const-app/static/js/5.db5bcd1f.chunk.js"
+  },
+  {
+    "revision": "0b95d33cd3c66c1e6282",
+    "url": "/const-app/static/js/6.b0901c50.chunk.js"
+  },
+  {
+    "revision": "04d19908cabc9b54fa3a",
+    "url": "/const-app/static/js/7.39be89f2.chunk.js"
+  },
+  {
+    "revision": "e93f1de37f2dbbaeb2a4",
+    "url": "/const-app/static/js/main.702516dd.chunk.js"
+  },
+  {
+    "revision": "3e04ea9c4e41b4c5b1e6",
+    "url": "/const-app/static/js/runtime-main.b8279cd5.js"
+  },
+  {
+    "revision": "aabda2b75a29893ccd29e93dcadc68cd",
+    "url": "/const-app/static/media/avatar.aabda2b7.png"
+  }
+]);
